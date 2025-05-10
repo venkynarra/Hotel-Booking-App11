8d3095cade7b5b39eb2398eb46b07f9861e477d7
venkatesh <venkateshnarra368@gmail.com>
2025-05-10 15:35:39 -0500

    Day 41- Third Commit-- Reservation ticket class, printing out the ticket.

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ df = pd.read_csv("hotels.csv", dtype = {"id": str})
 class Hotel:
     def __init__(self, hotel_id):
         self.hotel_id = hotel_id
+        self.name = df.loc[df["id"] == self.hotel_id, "name"].squeeze()
+
     def book(self):# book a hotel by changing its availability to no
         df.loc[df["id"] == self.hotel_id, "available"] = "no"
         df.to_csv("hotels.csv", index = False)
@@ -22,10 +24,16 @@ class Hotel:
 
 class ReservationTicket:
     def __init__(self,  customer_name, hotel_object):
-        pass
-    def generate(self):
-        pass
+        self.customer_name = customer_name
+        self.hotel = hotel_object
 
+    def generate(self):
+        content = f"""
+                Thank you for your reservation!
+             Here is the booking data:
+               Name: {self.customer_name}
+                hotel:{self.hotel.name}"""
+        return content
 
 print(df)
 
@@ -34,7 +42,7 @@ hotel = Hotel(hotel_ID)
 if hotel.available():
     hotel.book()
     name = input("Enter Your Name: ")
-    reservation_ticket = ReservationTicket(name, hotel)
+    reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
     print(reservation_ticket.generate())
 else:
     print("Hotel is not Free.")
